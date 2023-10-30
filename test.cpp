5823d128af9cea1387408dc6b8031d49a7c492bd
gueris arriaza <gueris01@hotmail.com>
2023-10-29 21:20:08 -0400

    Adding variable for window width and height and controls

--- test.cpp
+++ test.cpp
@@ -1,13 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+int windowWidth = 600;
+int windowHeight = 400;
+
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    RenderWindow window(VideoMode(windowWidth, windowHeight), "SFML works!");
     CircleShape shape(100.f);
     shape.setFillColor(Color::Red);
 
+    bool up, down, left, rihgt = false;
+    
     while (window.isOpen())
     {
         Event event;
